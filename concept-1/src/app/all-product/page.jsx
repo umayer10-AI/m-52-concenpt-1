aa6d43ecfcad7dfafcaec9263987adf6ee0e2693
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 20:15:19 +0600

    add

--- concept-1/src/app/all-product/page.jsx
+++ concept-1/src/app/all-product/page.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h2 className='text-3xl font-bold text-center my-5'>All Products</h2>
+            <h2 className='text-3xl font-semibold text-center my-5'>All Products</h2>
             <Carts></Carts>
         </div>
     );
--- concept-1/src/app/page.js
+++ concept-1/src/app/page.js
@@ -1,10 +1,19 @@
+import AllCards from '@/component/AllCards';
+import { getData } from '@/lib/data';
 import React from 'react';
 
-const page = () => {
+const page = async () => {
+
+  const data = await getData()
+
   return (
     <div>
-      <h2 className='text-3xl font-bold text-center my-10'>Hello Umayer</h2>
-      {/* <h2 className='text-3xl font-bold text-green-500 my-10'>Umayer Ahmad</h2> */}
+      <h2 className='text-3xl font-semibold my-5'>Popular Products</h2>
+      <div className='grid grid-cols-1 lg:grid-cols-4 gap-5'>
+            {
+                data.slice(8,12).map(v => <AllCards key={v._id} p={v}></AllCards>)
+            }
+        </div>
     </div>
   );
 };
