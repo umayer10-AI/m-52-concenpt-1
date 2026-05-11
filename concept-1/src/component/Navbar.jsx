1102d44e7723519f29b0ce4b2ca886feaaa832e4
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 19:32:35 +0600

    add

--- concept-1/src/component/Navbar.jsx
+++ concept-1/src/component/Navbar.jsx
@@ -3,9 +3,11 @@ import Nav1 from './Nav1';
 
 const Navbar = () => {
     return (
-        <div className=''>
-            <h2 className='text-2xl font-bold bg-linear-to-r from-green-500 to-blue-500 text-transparent bg-clip-text w-fit'>PH E-Commerce</h2>
-            <Nav1></Nav1>
+        <div className='border-b border-b-cyan-800 py-4'>
+            <div className='flex items-center justify-between w-[90%] mx-auto'>
+                <h2 className='text-2xl font-bold bg-linear-to-r from-green-500 to-blue-500 text-transparent bg-clip-text w-fit'>PH E-Commerce</h2>
+                <Nav1></Nav1>
+            </div>
         </div>
     );
 };
