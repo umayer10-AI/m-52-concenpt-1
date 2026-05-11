b1decd5b6ce5085caad0ecf6ba50eceed490ac93
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 19:55:45 +0600

    add

--- concept-1/src/lib/data.js
+++ concept-1/src/lib/data.js
@@ -2,3 +2,9 @@ export const getData = async () => {
     const res = await fetch(`http://localhost:5000/user`)
     return res.json()
 }
+
+export const getId = async (id) => {
+    const res = await fetch(`http://localhost:5000/user/${id}`)
+    const data = await res.json()
+    return data
+}
