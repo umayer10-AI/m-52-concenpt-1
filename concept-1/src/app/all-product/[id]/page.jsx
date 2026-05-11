9d6cb323bb4500337843fe5a54d9a3b67f137b49
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 20:05:03 +0600

    add

--- concept-1/src/app/all-product/[id]/page.jsx
+++ concept-1/src/app/all-product/[id]/page.jsx
@@ -24,18 +24,14 @@ const page = async ({params}) => {
         </div>
 
         {/* Right Side */}
-        <div className="p-8 flex flex-col justify-center space-y-6">
-          
+        <div className="p-8 flex flex-col justify-center space-y-6"> 
           <div>
-            <span className="bg-indigo-100 text-indigo-700 px-4 py-1 rounded-full text-sm font-semibold">
-              {product.category}
-            </span>
 
-            <h1 className="text-4xl font-bold text-gray-800 mt-4">
+            <h1 className="text-4xl font-bold bg-linear-to-r from-green-500 to-blue-500 text-transparent bg-clip-text w-fit mt-4">
               {product.title}
             </h1>
 
-            <p className="text-gray-600 mt-4 leading-relaxed">
+            <p className="text-gray-400 mt-4 leading-relaxed">
               {product.description}
             </p>
           </div>
@@ -46,14 +42,14 @@ const page = async ({params}) => {
               ★★★★★
             </div>
 
-            <span className="text-gray-600 font-medium">
+            <span className="text-cyan-500 font-medium">
               {product.rating} Rating
             </span>
           </div>
 
           {/* Price */}
           <div className="flex items-center gap-4">
-            <h2 className="text-4xl font-bold text-indigo-600">
+            <h2 className="text-4xl font-bold bg-linear-to-r from-green-500 to-blue-500 text-transparent bg-clip-text w-fit">
               ${product.price}
             </h2>
 
@@ -63,24 +59,15 @@ const page = async ({params}) => {
           </div>
 
           {/* Extra Info */}
-          <div className="space-y-2 text-gray-700">
-            <p>
-              <span className="font-semibold">Brand:</span> {product.brand}
-            </p>
 
-            <p>
-              <span className="font-semibold">Category:</span>{" "}
-              {product.category}
-            </p>
-          </div>
 
           {/* Buttons */}
           <div className="flex gap-4 pt-4">
-            <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-8 py-3 rounded-xl font-semibold transition duration-300">
+            <button className="bg-linear-to-r from-green-500 to-blue-600 hover:bg-indigo-700 text-white px-8 py-3 rounded-xl font-semibold transition duration-300">
               Add To Cart
             </button>
 
-            <button className="border border-indigo-600 text-indigo-600 hover:bg-indigo-50 px-8 py-3 rounded-xl font-semibold transition duration-300">
+            <button className="border border-cyan-600 bg-linear-to-r from-green-500 to-blue-500 text-transparent bg-clip-text w-fit hover:bg-indigo-50 px-8 py-3 rounded-xl font-semibold transition duration-300">
               Wishlist
             </button>
           </div>
