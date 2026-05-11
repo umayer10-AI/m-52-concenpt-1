deb0267e03bbae8f437d1c0c9eb10662ce4a509b
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 22:16:38 +0600

    add

--- server/index.js
+++ server/index.js
@@ -50,6 +50,11 @@ const run = async () => {
             res.send(result)
         })
 
+        app.post('/user', async (req,res) => {
+            const newUser = req.body
+            console.log(newUser)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
