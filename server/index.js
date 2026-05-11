47028db35c355955b736e731d57586265bdbe2c5
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 17:18:47 +0600

    add

--- server/index.js
+++ server/index.js
@@ -9,6 +9,37 @@ dotenv.config()
 app.use(cors())
 app.use(express.json())
 
+const uri = process.env.MY;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const db = client.db("umayer");
+        const usersCollection = db.collection("users");
+
+        app.get('/user', async (req,res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        });
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
   res.send('Hello World Umayer')
 })
