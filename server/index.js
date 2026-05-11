d1c7dff7f5ccd5c6724e8f0d25fae623625aa07d
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 17:10:26 +0600

    add

--- server/index.js
+++ server/index.js
@@ -2,21 +2,12 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 dotenv.config()
 
 app.use(cors())
 app.use(express.json())
-
-const data = [
-    {id: 1, device: 'Laptop', price: 1000},
-    {id: 2, device: 'Smartphone', price: 500},
-    {id: 3, device: 'Tablet', price: 300},
-]
-
-app.get('/user', (req, res) => {
-  res.send(data)
-})
 
 app.get('/', (req, res) => {
   res.send('Hello World Umayer')
