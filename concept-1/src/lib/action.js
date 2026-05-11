dbb14aa88f9cbb48e30c605fa92628594ab22b44
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 21:55:20 +0600

    add

--- concept-1/src/lib/action.js
+++ concept-1/src/lib/action.js
@@ -5,6 +5,7 @@ export const deleteUser = async (id) => {
         method: "DELETE"
     })
     const data = await res.json()
+    
     if(data.deletedCount > 0){
         alert("Delete Successfully")
         redirect('/all-product')
