f5db0d9cef805bb8b15f7c4c87d4eacdedb3b0f3
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 19:30:46 +0600

    add

--- concept-1/src/component/Nav1.jsx
+++ concept-1/src/component/Nav1.jsx
@@ -1,9 +1,40 @@
+"use client"
+import { Button } from '@heroui/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Nav1 = () => {
+
+    const p = usePathname()
+
+    const links = [
+        {
+            name: 'Home',
+            path: '/'
+        },
+        {
+            name: 'All Product',
+            path: '/all-product'
+        },
+        {
+            name: 'Add Product',
+            path: '/add-product'
+        },
+    ]
+
     return (
-        <div>
-            
+        <div className='text-sm flex items-center gap-7'>
+            {
+                links.map(v => (
+                    <Link className='font-semibold' key={v.name} href={v.path}>
+                        {
+                            p===v.path? <Button size='sm' className={`bg-linear-to-r from-green-500 to-blue-500`}>{v.name}</Button>
+                            : <button>{v.name}</button>
+                        }
+                    </Link>
+                ))
+            }
         </div>
     );
 };
--- concept-1/src/component/Navbar.jsx
+++ concept-1/src/component/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav1 from './Nav1';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2 className='text-2xl font-bold'>PH E-Commerce</h2>
+        <div className=''>
+            <h2 className='text-2xl font-bold bg-linear-to-r from-green-500 to-blue-500 text-transparent bg-clip-text w-fit'>PH E-Commerce</h2>
+            <Nav1></Nav1>
         </div>
     );
 };
