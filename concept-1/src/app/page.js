ad79c05b6bf5453a8e3a2a0f48bbf12914b3832e
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 16:40:44 +0600

    add

--- concept-1/src/app/page.js
+++ concept-1/src/app/page.js
@@ -3,7 +3,8 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      <dib >Hello World</dib>
+      <h2 className='text-3xl font-bold text-center my-10'>Hello Umayer</h2>
+      <h2>Hello Umayer</h2>
     </div>
   );
 };
