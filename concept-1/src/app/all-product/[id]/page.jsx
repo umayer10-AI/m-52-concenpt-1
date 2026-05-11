5780b6ee2c8d4aed6eea4e83a405c0923c16bbe3
umayer10-AI <mdumayer10@gmail.com>
2026-05-11 19:56:52 +0600

    add

--- concept-1/src/app/all-product/[id]/page.jsx
+++ concept-1/src/app/all-product/[id]/page.jsx
@@ -1,6 +1,12 @@
+import { getId } from '@/lib/data';
 import React from 'react';
 
-const page = () => {
+const page = async ({params}) => {
+
+    const {id} = await params
+    const data = await getId(id)
+    // console.log(data)
+
     return (
         <div>
             
